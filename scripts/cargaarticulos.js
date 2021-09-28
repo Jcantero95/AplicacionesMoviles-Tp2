f5ecc552ac2d72a5a1caaaa632de0c60bb93f7b6
Martin G <martin.a.gimenez@gmail.com>
2021-09-28 02:53:24 -0300

    traer los datos del JSON en index2

--- scripts/cargaarticulos.js
+++ scripts/cargaarticulos.js
@@ -1,6 +1,74 @@
 $(document).ready(function(){
-    $.getJSON('generated.json', function(dato, status, xhr){
-        console.log("datos: " + dato +"\nStatus: " + " " + xhr.status);
-    })
+
+    $("#botonComic").click(function(){
+        $.ajax({
+            url: "./scripts/generated.json",
+            type: "GET",
+            dataType: "json",
+            success: function(datos){
+                $("#info").empty();
+                $.each(datos, function(index, obj){
+                    if(obj.categoria == "Comic"){
+                        
+                        $("#info").append("<article class='Producto'>" + "<img src=" + obj.picture + " width='200' height='200'></img>" + "<h4>" + obj.articulo + "</h4>" + "<h4>$" + obj.precio + "</h4>" + "<p>" + obj.descripcion + "</article>")
+                    }      
+            
+            })}
+            
+        })
+    }) 
+
     
+    $("#botonBandas").click(function(){
+        $.ajax({
+            url: "./scripts/generated.json",
+            type: "GET",
+            dataType: "json",
+            success: function(datos){
+                $("#info").empty();
+                    $.each(datos, function(index, obj){
+    
+                        if(obj.categoria == "Bandas"){
+                            
+                            $("#info").append("<article class='Producto'>" + "<img src=" + obj.picture + " width='200' height='200'></img>" + "<h4>" + obj.articulo + "</h4>" + "<h4>$" + obj.precio + "</h4>" + "<p>" + obj.descripcion + "</article>")
+                        }
+                })
+            }   
+        })
+    })
+
+    $("#botonDeportes").click(function(){
+        $.ajax({
+            url: "./scripts/generated.json",
+            type: "GET",
+            dataType: "json",
+            success: function(datos){
+                $("#info").empty();
+                    $.each(datos, function(index, obj){
+    
+                        if(obj.categoria == "Deportes"){
+                            $("#info").append("<article class='Producto'>" + "<img src=" + obj.picture + " width='200' height='200'></img>" + "<h4>" + obj.articulo + "</h4>" + "<h4>$" + obj.precio + "</h4>" + "<p>" + obj.descripcion + "</article>")
+                        }
+                })
+            }   
+        })
+    })
+
+    $("#botonAcc").click(function(){
+        $.ajax({
+            url: "./scripts/generated.json",
+            type: "GET",
+            dataType: "json",
+            success: function(datos){
+                $("#info").empty();
+                    $.each(datos, function(index, obj){
+    
+                        if(obj.categoria == "Accesorios"){
+                            console.log(obj);
+                            $("#info").append("<article class='Producto'>" + "<img src=" + obj.picture + " width='200' height='200'></img>" + "<h4>" + obj.articulo + "</h4>" + "<h4>$" + obj.precio + "</h4>" + "<p>" + obj.descripcion + "</article>")
+                        }
+                })
+            }   
+        })
+    })
 })
